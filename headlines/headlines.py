13a3cc64bb014e0969b1dede1c013dfb2e2419d5
bbptx <26070077+bbptx@users.noreply.github.com>
2018-08-01 11:59:10 -0500

    Using GETE

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,6 +1,7 @@
 import feedparser
-from flask import render_template
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,16 +10,28 @@ RSS_FEEDS = {"bbc": "http://feeds.bbci.co.uk/news/rss.xml",
 			 "fox": "http://feeds.foxnews.com/foxnews/latest" }
 			 
 @app.route("/")
-@app.route("/<publication>")
+#@app.route("/<publication>")
 
+def get_news():
+	query = request.args.get("publication")
+	if not query or query.lower() not in RSS_FEEDS:
+		publication="bbc"
+	else:
+		publication = query.lower()
+	
+	feed = feedparser.parse(RSS_FEEDS[publication])
+	
+	return render_template("home.html", publication=publication, articles=feed['entries'])
 
+	
+"""
 def get_news(publication="bbc"):
 	feed = feedparser.parse(RSS_FEEDS[publication])
 	first_article = feed['entries'][0]
 	
 	return render_template("home.html", publication=publication, articles=feed['entries'])
 	
-	""" 
+	""""""
 	return render_template("home.html", publication=publication, article=first_article)
 	
 	
@@ -35,7 +48,9 @@ def get_news(publication="bbc"):
 			<p>{3}</p> <br/>
 		</body>
 	</html>.format(publication, first_article.get("title"), first_article.get("published"), first_article.get("summary")   )
-	""" 
+	"""""" 
+
+"""
 	
 
 if __name__ == "__main__":
